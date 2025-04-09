db27798053c9dccb6764107f06682af83886e257
Max YT <161175370+MaxYT5184@users.noreply.github.com>
2025-04-09 18:55:40 -0400

    Delete js directory

--- js/theme.js
+++
@@ -1,6 +0,0 @@
-const themeToggle = document.getElementById('themeToggle');
-themeToggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-});
-
-document.body.style.transition = 'background-color 0.3s ease';
